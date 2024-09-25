7cec53a8e4243fa0bb9d1eb9bc45156ac494b63c
jengledow <jmengledow5@gmail.com>
2024-09-24 23:46:41 -0400

    added another page

---
+++ resources/js/Pages/Budget.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+    import SecondaryButton from '@/Components/SecondaryButton.vue';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-white leading-tight">Budget</h2>
+        </template>
+
+        <div>
+            <SecondaryButton>
+                Set Budget
+            </SecondaryButton>
+        </div>
+    </AuthenticatedLayout>
+</template>
